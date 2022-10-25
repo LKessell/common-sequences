ed2f1fcfc9dda991d320791ce0ca4a0248d4d2e8
Lauren Kessell <lkessell1@gmail.com>
2022-10-25 15:08:23 -0400

    Add function to track instances of three word triplets

--- solution.js
+++ solution.js
@@ -9,12 +9,25 @@ function formatFile(path) {
   const lowercased = punctuationless.toLowerCase();
   const singleSpaced = lowercased.replace(/  +/g, " ");
 
-  console.log(singleSpaced.split(" ").slice(0, 50));
-  return singleSpaced.split(" ");
+  // console.log(singleSpaced.split(" ").slice(0, 50));
+  // return singleSpaced.split(" ");
+  return singleSpaced.split(" ").slice(0, 50);
+}
+
+function trackTripleInstances(list, key) {
+  for (let i = 0; i < list.length - 3; i++) {
+    const triple = `${list[i]} ${list[i + 1]} ${list[i + 2]}`;
+    if (!key[triple]) key[triple] = 0;
+    key[triple]++;
+  }
 }
 
 function findTopTriplets() {
-  formatFile(process.argv[2]);
+  const key = {};
+
+  // formatFile(process.argv[2]);
+  trackTripleInstances(formatFile(process.argv[2]), key);
+  console.log(key);
 }
 
 findTopTriplets();
