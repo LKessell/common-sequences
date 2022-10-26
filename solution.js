43bef0432962914375dc6aa107e10c7d9ddae51c
Lauren Kessell <lkessell1@gmail.com>
2022-10-26 13:57:30 -0400

    Add variables for legibility, tweak formatFile

--- solution.js
+++ solution.js
@@ -2,10 +2,8 @@ const { readFileSync } = require("fs");
 
 function formatFile(path) {
   const rawContent = readFileSync(path, "utf-8");
-  const contentByLine = rawContent.split(/\r?\n/);
-  const punctuationless = contentByLine
-    .join(" ")
-    .replace(/[^\p{L}\p{N}\s]/gu, "");
+  const noLineBreaks = rawContent.split(/\r?\n/).join(" ");
+  const punctuationless = noLineBreaks.replace(/[^\p{L}\p{N}\s]/gu, "");
   const lowercased = punctuationless.toLowerCase();
   const singleSpaced = lowercased.replace(/  +/g, " ");
 
@@ -15,6 +13,7 @@ function formatFile(path) {
 function trackTripletInstances(list, key) {
   for (let index = 0; index <= list.length - 3; index++) {
     const triplet = `${list[index]} ${list[index + 1]} ${list[index + 2]}`;
+
     if (!key[triplet]) key[triplet] = 0;
     key[triplet]++;
   }
@@ -29,14 +28,18 @@ function findTopTriplets() {
 
   process.argv.forEach((path) => {
     if (!path.includes(".txt")) return;
-    else trackTripletInstances(formatFile(path), key);
+    else {
+      const formattedText = formatFile(path);
+      trackTripletInstances(formattedText, key);
+    }
   });
 
   const sortedInstances = Object.entries(key).sort((a, b) => b[1] - a[1]);
   const topHundred = sortedInstances.slice(0, 100);
+  const formattedSolution = formatSolution(topHundred);
 
-  console.log(formatSolution(topHundred));
-  return formatSolution(topHundred);
+  console.log(formattedSolution);
+  return formattedSolution;
 }
 
 findTopTriplets();
