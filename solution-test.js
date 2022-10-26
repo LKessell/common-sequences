c652a23565c5f01ccb021650b450aeae0830bd9c
Lauren Kessell <lkessell1@gmail.com>
2022-10-25 20:06:01 -0400

    Add test for trackTripletInstances

--- solution-test.js
+++ solution-test.js
@@ -27,4 +27,36 @@ describe("solution.js", function () {
       ]);
     });
   });
+
+  describe("trackTripletInstances", function () {
+    it("Should be a function", function () {
+      assert.isFunction(trackTripletInstances);
+    });
+
+    it("Should take in a list of words and a key; instances of three word sequences should be tracked in the key", function () {
+      const key = {};
+      const sampleList = [
+        "i",
+        "do",
+        "not",
+        "this",
+        "list",
+        "i",
+        "do",
+        "not",
+        "list",
+      ];
+
+      trackTripletInstances(sampleList, key);
+
+      assert.deepEqual(key, {
+        "i do not": 2,
+        "do not this": 1,
+        "not this list": 1,
+        "this list i": 1,
+        "list i do": 1,
+        "do not list": 1,
+      });
+    });
+  });
 });
